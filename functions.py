3df52c9095c9525011264158ec7042b2c383eae2
javlon <jumrzokov@gmail.com>
2026-09-11 18:36:34 +0900

    feat: learn functions

---
+++ functions.py
@@ -0,0 +1,17 @@
+
+# Define
+def greet(a):
+    print(f"How do you do, {a}")
+
+
+def greeting(b):
+    print('greeting is executed!')
+    return f"Hi, {b}"
+
+
+# Call
+result1 = greet("Jack")
+print("result1: ", result1)
+
+result2 = greeting("John")
+print("result2: ", result2)
