6113fea9cd17eb6b74b382ec935ac3d39af65437
javlon <jumrzokov@gmail.com>
2026-09-11 17:23:06 +0900

    feat: python variuables

--- run.py
+++ run.py
@@ -1,2 +1,12 @@
-message = "Hello world!"
+# Dunder __builtins__, __init__
+message = "In Python Everything is object!"
 print(message)
+result = type(message)
+print("result", result)
+
+''' In python there are built in tools:
+Types: int float str list dict
+Funtctions: print() len() inpput() type() str() int()
+Constants: TRUE FALSE NONE
+'''
+print(dir(__builtins__))
